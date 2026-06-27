b9f8784729408b7684aa1deb0c6c76e204a573f2
ganatudomingo <ganatudomingo@gmail.com>
2026-06-26 22:10:57 -0700

    GTD3.0

---
+++ sw.js
@@ -0,0 +1,44 @@
+var CACHE = 'gtd-v1';
+var URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(URLS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(resp) {
+        return caches.open(CACHE).then(function(cache) {
+          cache.put(e.request, resp.clone());
+          return resp;
+        });
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
+});
